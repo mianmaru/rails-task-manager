bd7daaf69a949e279f9f5781b31e6b6167d57fe0
Miguel Angel Marfil Rubio <mianmaru@gmail.com>
2019-11-26 18:39:09 +0100

    finish with the last refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -18,17 +19,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(user_params)
     redirect_to task_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -36,6 +34,10 @@ class TasksController < ApplicationController
  private
   def user_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_restaurant
+    @task = Task.find(params[:id])
   end
 end
 
